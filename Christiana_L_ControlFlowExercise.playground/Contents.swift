a2c82b3bffa67bc1749b9c4b374cc7570271fffd
Chrissy1025 <67969110+Chrissy1025@users.noreply.github.com>
2020-07-14 11:27:45 -0500

    Add files via upload

---
+++ Christiana_L_ControlFlowExercise.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let Welcome = "Welcome to my Egg Timer"
+for index in 1...6 {
+    print ("\(index) times 30 is \(index * 30)")
+}
+
+//When Timer Stops
+let timer =  180
+
+if timer == 180 {
+    print("Timer Stopped")
+}else {
+    print("Timer Start")
+}
+
